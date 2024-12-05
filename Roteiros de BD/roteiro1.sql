9d09245f68bded5e08bad8fd570a4e9b95fdc033
Neto <antoniobarros3583@gmail.com>
2024-12-05 17:09:55 -0300

    finalização do roteiro01 até a parte 9

--- Roteiros de BD/roteiro1.sql
+++ Roteiros de BD/roteiro1.sql
@@ -1,82 +1,6 @@
+-- Questão 1 e 2
 
--- Questão 1:
-
--- Adicionando atributos que julgo ser essencial para cada tabela
-
-/*
-Automovel (
-    renavam,
-    placa,
-    modelo,
-    cor,
-    dono_automovel 
-)
-
-Segurado (
-    cpf,
-    nome,
-    telefone,
-    email,
-    cep
-)
-
-Perito (
-    cpf_perito,
-    nome_perito,
-    telefone_perito,
-    email_perito
-)
-
-Oficina (
-    cnpj_oficina,
-    nome_oficina,
-    endereco,
-    telefone_oficina
-)
-
-Seguro (
-    id_seguro,
-    tipo_seguro,
-    renavam_automovel,
-    cpf_segurado,
-    data_inicio,
-    data_fim,
-    apolice
-)
-
-Sinistro (
-    id_sinistro,
-    id_seguro,
-    renavam_automovel,
-    data_ocorrencia,
-    local_ocorrencia,
-    descricao,
-    tipo_sinistro
-)
-
-Pericia (
-    id_pericia,
-    perito,
-    oficina,
-    seguro,
-    data_hora_inicio,
-    data_hora_fim,
-    conclusão
-)
-
-Reparo (
-    id_reparo,
-    id_seguro
-    oficina,
-    inicio_reparo,
-    fim_reparo,
-    valor
-)
-*/
-
--- Questão 2
-
---Criando tabelas com cada atributo previamente defenidos sem o uso de constrains
+--Criando tabelas com cada atributo defenidos sem o uso de constrains
 
 CREATE TABLE Automovel (
     renavam VARCHAR(11),
@@ -106,7 +30,7 @@ CREATE TABLE Oficina(
     cnpj_oficina VARCHAR(14),
     nome_oficina TEXT,
     endereco TEXT,
-    telefone_oficina text
+    telefone_oficina TEXT
 );
 
 CREATE TABLE Seguro (
@@ -130,18 +54,18 @@ CREATE TABLE Sinistro (
 
 CREATE TABLE Pericia (
     id_pericia INTEGER,
-    seguro INTEGER,
-    perito VARCHAR(11),
-    oficina VARCHAR(14),
+    id_seguro INTEGER,
+    cpf_perito VARCHAR(11),
+    cnpj_oficina VARCHAR(14),
     data_hora_inicio TIMESTAMP,
     data_hora_fim TIMESTAMP,
-    conclusão TEXT
+    conclusao TEXT
 );
 
 CREATE TABLE Reparo (
     id_reparo INTEGER,
     id_seguro INTEGER,
-    oficina VARCHAR(14),
+    cnpj_oficina VARCHAR(14),
     inicio_reparo DATE,
     fim_reparo DATE,
     valor NUMERIC
@@ -149,8 +73,7 @@ CREATE TABLE Reparo (
 
 -- Questão 3
 
---Definindo as chaves primárias das tabelas e adicionando as 
---constrains através do ALTER TABLE 
+--Definindo as chaves primárias das tabelas
 
 ALTER TABLE Automovel ADD PRIMARY KEY (renavam);
 ALTER TABLE Segurado ADD PRIMARY KEY (cpf_segurado);
@@ -163,7 +86,7 @@ ALTER TABLE Reparo ADD PRIMARY KEY (id_reparo);
 
 -- Questão 4
 
---Definindo chaves estrangeiras
+--Definindo chaves estrangeiras das tabelas
 
 ALTER TABLE Automovel ADD FOREIGN KEY (cpf_dono) REFERENCES Segurado (cpf_segurado);
 
@@ -172,14 +95,16 @@ ALTER TABLE Seguro ADD FOREIGN KEY (cpf_segurado) REFERENCES Segurado (cpf_segur
 
 ALTER TABLE Sinistro ADD FOREIGN KEY (id_seguro) REFERENCES Seguro (id_seguro);
 
-ALTER TABLE Pericia ADD FOREIGN KEY (perito) REFERENCES Perito (cpf_perito);
---->ALTER TABLE Pericia ADD FOREIGN KEY (seguro) REFERENCES Seguro (id_seguro);
-ALTER TABLE Pericia ADD FOREIGN KEY (oficina) REFERENCES Oficina (cnpj_oficina);
+ALTER TABLE Pericia ADD FOREIGN KEY (cpf_perito) REFERENCES Perito (cpf_perito);
+ALTER TABLE Pericia ADD FOREIGN KEY (id_seguro) REFERENCES Seguro (id_seguro);
+ALTER TABLE Pericia ADD FOREIGN KEY (cnpj_oficina) REFERENCES Oficina (cnpj_oficina);
 
 ALTER TABLE Reparo ADD FOREIGN KEY (id_seguro) REFERENCES Seguro (id_seguro);
-ALTER TABLE Reparo ADD FOREIGN KEY (oficina) REFERENCES oficina (cnpj_oficina);
+ALTER TABLE Reparo ADD FOREIGN KEY (cnpj_oficina) REFERENCES oficina (cnpj_oficina);
 
 --Questão 5
+
+-- Definindo quais atributos não podem ser null na tabela
 
 ALTER TABLE Automovel ALTER COLUMN placa SET NOT NULL;
 ALTER TABLE Automovel ALTER COLUMN modelo SET NOT NULL;
@@ -187,7 +112,7 @@ ALTER TABLE Automovel ALTER COLUMN cpf_dono SET NOT NULL;
 
 ALTER TABLE Segurado ALTER COLUMN nome_segurado SET NOT NULL;
 ALTER TABLE Segurado ALTER COLUMN email_segurado SET NOT NULL;
--- talvez coloque o telefone do segurado
+ALTER TABLE Segurado ALTER COLUMN telefone_segurado SET NOT NULL;
 ALTER TABLE Segurado ALTER COLUMN cep_segurado SET NOT NULL;
 
 ALTER TABLE Perito ALTER COLUMN nome_perito SET NOT NULL;
@@ -200,10 +125,30 @@ ALTER TABLE Oficina ALTER COLUMN telefone_oficina SET NOT NULL;
 
 ALTER TABLE Seguro ALTER COLUMN renavam_automovel SET NOT NULL;
 ALTER TABLE Seguro ALTER COLUMN cpf_segurado SET NOT NULL;
+ALTER TABLE Seguro ALTER COLUMN data_inicio SET NOT NULL;
+ALTER TABLE Seguro ALTER COLUMN data_fim SET NOT NULL;
 ALTER TABLE Seguro ALTER COLUMN apolice SET NOT NULL;
 
+ALTER TABLE Sinistro ALTER COLUMN id_seguro SET NOT NULL;
+ALTER TABLE Sinistro ALTER COLUMN renavam_automovel SET NOT NULL;
+ALTER TABLE Sinistro ALTER COLUMN tipo_sinistro SET NOT NULL;
+ALTER TABLE Sinistro ALTER COLUMN data_ocorrencia SET NOT NULL;
+ALTER TABLE Sinistro ALTER COLUMN local_ocorrencia SET NOT NULL;
 
--- Questão 6
+ALTER TABLE Pericia ALTER COLUMN id_seguro SET NOT NULL;
+ALTER TABLE Pericia ALTER COLUMN cpf_perito SET NOT NULL;
+ALTER TABLE Pericia ALTER COLUMN cnpj_oficina SET NOT NULL;
+ALTER TABLE Pericia ALTER COLUMN data_hora_inicio SET NOT NULL;
+ALTER TABLE Pericia ALTER COLUMN data_hora_fim SET NOT NULL;
+ALTER TABLE Pericia ALTER COLUMN conclusao SET NOT NULL;
+
+ALTER TABLE Reparo ALTER COLUMN id_seguro SET NOT NULL;
+ALTER TABLE Reparo ALTER COLUMN cnpj_oficina SET NOT NULL;
+ALTER TABLE Reparo ALTER COLUMN inicio_reparo SET NOT NULL;
+ALTER TABLE Reparo ALTER COLUMN fim_reparo SET NOT NULL;
+ALTER TABLE Reparo ALTER COLUMN valor SET NOT NULL;
+
+-- Questão 6 e 9
 DROP TABLE Pericia;
 DROP TABLE PERITO;
 DROP TABLE REPARO;
@@ -213,18 +158,89 @@ DROP TABLE SEGURO;
 DROP TABLE Automovel;
 DROP TABLE Segurado;
 
---Questão 7
+--Questão 7 E 8
 
---Questão 8
+/*
+CREATE TABLE Automovel (
+    renavam_automovel PRIMARY KEY,
+    placa NOT NULL,
+    modelo NOT NULL,
+    ano_automovel,
+    cor_automovel,
+    cpf_dono NOT NULL,
+    FOREIGN KEY (cpf_dono) REFERENCES Segurado (cpf_segurado)
+); 
 
--- Questão 9
-DROP TABLE Pericia;
-DROP TABLE PERITO;
-DROP TABLE REPARO;
-DROP TABLE Sinistro;
-DROP TABLE Oficina;
-DROP TABLE SEGURO;
-DROP TABLE Automovel;
-DROP TABLE Segurado;
+CREATE TABLE Segurado(
+    cpf_segurado PRIMARY KEY,
+    nome_segurado NOT NULL,
+    telefone_segurado NOT NULL,
+    email_segurado NOT NULL,
+    cep_segurado NOT NULL
+);
 
---Questão 10
+CREATE TABLE Perito (
+    cpf_perito PRIMARY KEY,
+    nome_perito NOT NULL,
+    telefone_perito NOT NULL,
+    email_perito NOT NULL
+);
+
+CREATE TABLE Oficina(
+    cnpj_oficina PRIMARY KEY,
+    nome_oficina NOT NULL,
+    endereco NOT NULL,
+    telefone_oficina NOT NULL
+);
+
+CREATE TABLE Seguro (
+    id_seguro PRIMARY KEY,
+    renavam_automovel NOT NULL,
+    cpf_segurado NOT NULL,
+    data_inicio NOT NULL,
+    data_fim NOT NULL,
+    apolice NOT NULL,
+    FOREIGN KEY (cpf_segurado) REFERENCES SEGURADO (cpf_segurado),
+    FOREIGN KEY (renavam_automovel) REFERENCES Automovel (renavam_automovel)
+);
+
+CREATE TABLE Sinistro (
+    id_sinistro PRIMARY KEY,
+    id_seguro NOT NULL,
+    renavam_automovel NOT NULL,
+    data_ocorrencia NOT NULL,
+    local_ocorrencia NOT NULL,
+    descricao,
+    tipo_sinistro NOT NULL,
+    FOREIGN KEY (id_seguro) REFERENCES Seguro (id_seguro),
+    FOREIGN KEY (renavam_automovel) REFERENCES Automovel (renavam_automovel)
+);
+
+CREATE TABLE Pericia (
+    id_pericia PRIMARY KEY,
+    id_seguro NOT NULL,
+    cpf_perito NOT NULL,
+    cnpj_oficina NOT NULL,
+    data_hora_inicio NOT NULL,
+    data_hora_fim NOT NULL,
+    conclusao NOT NULL,
+    FOREIGN KEY (id_seguro) REFERENCES Seguro (id_seguro),
+    FOREIGN KEY (cpf_perito) REFERENCES Perito (cpf_perito),
+    FOREIGN KEY (cnpj_oficina) REFERENCES Oficina (cnpj_oficina)
+);
+
+CREATE TABLE Reparo (
+    id_reparo PRIMARY KEY,
+    id_seguro NOT NULL,
+    cnpj_oficina NOT NULL,
+    renavam_automovel NOT NULL,
+    data_inicio_reparo NOT NULL,
+    data_fim_reparo NOT NULL, 
+    valor NOT NULL
+    FOREIGN KEY (id_seguro) REFERENCES Seguro (id_seguro),
+    FOREIGN KEY (cnpj_oficina) REFERENCES Oficina (cnpj_oficina),
+    FOREIGN KEY (renavam_automovel) REFERENCES Automovel (renavam_automovel)
+);
+*/
+
+-- Questão 10
